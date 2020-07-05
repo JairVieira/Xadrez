15cc56ffaa02c744f8c18c25a7010b3307f2a088
Jair Vieira <jairvieira13@gmail.com>
2020-07-05 20:01:16 -0300

    Implementando tabuleiro

---
+++ Xadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace Xadrez.Tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            Posicao = posicao;
+            Tab = tab;
+            Cor = Cor;
+            QtdMovimentos = 0
+        }
+    }
+}
